1aeda5000b9d38762064b6fb0d40ec643d67a3f1
JolyonForsyte <ali-beg@mail.ru>
2018-11-27 22:09:52 +0100

    new files

---
+++ Lesson1/Cycle.java
@@ -0,0 +1,27 @@
+public class Cycle{
+	public static void main(String[] args){
+
+		for(int i = 0;i<=20;i++){
+			System.out.print(i+ " ");
+		}
+		System.out.println("  ");
+		
+		int a = -6;
+		while(a<=6){
+			System.out.print(a + " ");
+			a+=2;
+		}
+		
+		System.out.println("  ");
+
+		int b=10;
+		int result = 0;
+		do{
+			if(b%2!=0){
+				result = result+b;
+			}
+			b++;
+		}while(b<=20);
+		System.out.println("Sum of odd numbers from 10 till 20 is " + result);
+	}
+}

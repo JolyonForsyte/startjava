6a4fe55a031dd7f9f4511c3a6ca172dd89911e34
JolyonForsyte <ali-beg@mail.ru>
2018-12-08 16:32:29 +0100

    changes to Jaeger Classes

--- Lesson2/Jaeger.java
+++ Lesson2/Jaeger.java
@@ -1,44 +1,39 @@
 public class Jaeger {
-
 	private String name;
 	private float height;
 	private float weight;
 
+	public Jaeger(String name, float height, float weight) {
+		super();
+		this.name = name;
+		this.height = height;
+		this.weight = weight;
+	}
 	public String getName() {
 		return name;
 	}
-
 	void setName(String name) {
 		this.name = name;
 	}
-
 	public float getHeight() {
 		return height;
 	}
-
 	public void setHeight(float height) {
 		this.height = height;
 	}
-
 	public float getWeight() {
 		return weight;
 	}
-
 	public void setWeight(float weight) {
 		this.weight = weight;
 	}
-
 	void useWeapon() {
 		System.out.println("Using cannons");
 	}
-
 	void move() {
-
 	}
-
 	@Override
 	public String toString() {
 		return "Jaeger [name=" + name + ", height=" + height + ", weight=" + weight + "]";
 	}
-
 }
--- Lesson2/JaegerTest.java
+++ Lesson2/JaegerTest.java
@@ -1,24 +1,26 @@
 public class JaegerTest {
-
 	public static void main(String[] args) {
+		Jaeger bracer = new Jaeger("Bracer Phoenix", 70.91f, 2.125f);
+		bracer.setWeight(2.147f);
+		System.out.println(bracer.toString());
+		
+		Jaeger striker = new Jaeger("Striker Eureka",76.2f,1.850f);
+		System.out.println(striker.toString());
+		/*
+		Jaeger bracer = new Jaeger();
+		bracer.setName("Bracer Phoenix");
+		bracer.setWeight(2.125f);
+		bracer.setHeight(70.91f);
+		System.out.println(bracer.toString());
+		bracer.useWeapon();
+		bracer.setWeight(2.151f);
+		System.out.println("JagerOne new weight " + bracer.getWeight() + " tonnes");
 
-		Jaeger jaegerOne = new Jaeger();
-		Jaeger jaegerTwo = new Jaeger();
-
-		jaegerOne.setName("Bracer Phoenix");
-		jaegerOne.setWeight(2.125f);
-		jaegerOne.setHeight(70.91f);
-		System.out.println(jaegerOne.toString());
-		jaegerOne.useWeapon();
-
-		jaegerOne.setWeight(2.151f);
-		System.out.println("JagerOne new weight " + jaegerOne.getWeight() + " tonnes");
-
-		jaegerTwo.setName("Striker Eureka");
-		jaegerTwo.setWeight(1.850f);
-		jaegerTwo.setHeight(76.2f);
-		System.out.println(jaegerTwo.toString());
-
+		Jaeger striker = new Jaeger();
+		striker.setName("Striker Eureka");
+		striker.setWeight(1.850f);
+		striker.setHeight(76.2f);
+		System.out.println(striker.toString());
+		*/
 	}
-
 }

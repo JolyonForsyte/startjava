0d7b5ce4f75ee883356983d5024213a47198e117
JolyonForsyte <ali-beg@mail.ru>
2018-12-11 22:19:13 +0100

    Calculator Version2

--- Lesson2/Calculator.java
+++ Lesson2/Calculator.java
@@ -1,31 +1,28 @@
 public class Calculator {
-
 	public static double calculate(int x, int y, char sign) {
 		double result = 0;
-
 		switch (sign) {
-		case '+':
-			result = x + y;
-			break;
-		case '-':
-			result = x - y;
-			break;
-		case '/':
-			result = x / y;
-			break;
-		case '*':
-			result = x * y;
-			break;
-		case '%':
-			result = x % y;
-			break;
-		case '^':
-			result = calculatePower(x, y);
-			break;
+			case '+':
+				result = x + y;
+				break;
+			case '-':
+				result = x - y;
+				break;
+			case '/':
+				result = x / y;
+				break;
+			case '*':
+				result = x * y;
+				break;
+			case '%':
+				result = x % y;
+				break;
+			case '^':
+				result = calculatePower(x, y);
+				break;
 		}
 		return result;
 	}
-
 	public static int calculatePower(int base, int power) {
 		int resultNumber = 1;
 		for (int i = 1; i <= power; i++) {
@@ -33,5 +30,4 @@ public class Calculator {
 		}
 		return resultNumber;
 	}
-
 }
--- Lesson2/CalculatorTest.java
+++ Lesson2/CalculatorTest.java
@@ -1,26 +1,34 @@
 import java.util.Scanner;
 
 public class CalculatorTest {
-
 	public static void main(String[] args) {
 
-		Calculator calcOne = new Calculator();
-
+		Scanner sc = new Scanner(System.in);
+		Calculator calc1 = new Calculator();
 		int number1;
 		int number2;
 		char operation;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the first number: ");
-		number1 = sc.nextInt();
+		String answer;
 
-		System.out.println("Enter the operation sign: ");
-		operation = sc.next().charAt(0);
+		while (true) {
+			System.out.println("Do you want to continue? enter yes or no");
+			Scanner ans = new Scanner(System.in);
+			answer = ans.nextLine();
 
-		System.out.println("Enter the second number: ");
-		number2 = sc.nextInt();
-
-		System.out.println(calcOne.calculate(number1, number2, operation));
-
+			if (answer.equals("yes")) {
+				System.out.println("Enter first number: ");
+				number1 = sc.nextInt();
+				System.out.println("Enter sign: ");
+				operation = sc.next().charAt(0);
+				System.out.println("Enter second number: ");
+				number2 = sc.nextInt();
+				System.out.println(calc1.calculate(number1, number2, operation));
+			} else if (answer.equals("no")) {
+				System.out.print("Programm ended");
+				break;
+			} else {
+				System.out.print("Invalid answer. ");
+			}
+		}
 	}
-
 }
